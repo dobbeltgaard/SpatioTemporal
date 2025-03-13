06444756be96c6717bea3f12f0f46fd11cdc2ccc
dobbeltgaard <albert.bisgaard@gmail.com>
2025-03-13 14:43:34 +0100

    updates

--- 01_preprocessing_master_data.R
+++ 01_preprocessing_master_data.R
@@ -4,8 +4,11 @@ library(readxl)
 library(stringr)
 library(dplyr)
 
-path = "C:/Users/ATBD/OneDrive - COWI/Documents - A235142 - Predictive maintanance ph.d project (1502)/Data/BDK/"
-#path = "C:/Users/atbd/COWI/A235142 - Predictive maintanance ph.d project (1502) - Documents/Data/BDK/"
+path = "C:/Users/atbd/COWI/A235142 - Predictive maintanance ph.d project (1502) - Documents/Data/BDK/"
+#path = "C:/Users/ATBD/OneDrive - COWI/Documents - A235142 - Predictive maintanance ph.d project (1502)/Data/BDK/"
+#path = "C:/Users/askbi/OneDrive - COWI/Documents - A235142 - Predictive maintanance ph.d project (1502)/Data/BDK/"
+#path = "D:/OneDrive - COWI/Documents - A235142 - Predictive maintanance ph.d project (1502)/Data/BDK/"
+
 
 asset.files = list.files(paste0(path, "Asset Data"), full.names = T)
 
@@ -44,6 +47,7 @@ SC$BTRn = as.numeric(SC$BTR)
 RosRing = NULL
 BTRs = as.vector(unique(R[R$BTRn >= 14000 & R$BTRn <= 14058,"BTR"]))$BTR #loop through these. Will happen later.
 #btr = "014034" #"014043" # # #We start by looking at one BTR. Later, we should loop through BTRs.
+#BTRs = as.numeric(BTRs)
 for(btr in BTRs){
   R1 = R[R$BTR == btr,] #BTR level 
   tracks = unique(R1$Spor)
@@ -114,68 +118,89 @@ for(btr in BTRs){
       statics = X, 
       meas = list()
     )
-    
   }
-  
 }
+names(RosRing) = as.numeric(names(RosRing))
+
 
 ###################################################
 ### ALL VARIABLES AND THEIR SPATIAL RESOLUTIONS ###
 ###################################################
+destination = "C:/Users/ATBD/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/database"
 
 PATTERNS = c("corr", "vrid", "hojd_L","hojd_R","overhojd", "wear", "rail", "side", "spor")
 var.idx = apply(sapply(PATTERNS, FUN = grepl, x = list.files(path), ignore.case = T),MARGIN = 1, FUN = any)
 variables = data.frame( name = c(list.files(path)[var.idx], unique(sub("_Campaign.*", "", list.files(paste0(path,"BDK_Noise_all"))))), 
                         resolution = NA, n_files = NA)
 
-all.all.files = list.files(path, recursive = T, full.names = T)
+all.all.files = list.files(destination, recursive = T, full.names = T)
 
 for(i in 1:NROW(variables)){
   files.of.variable = all.all.files[grepl(variables$name[i], all.all.files, ignore.case = T)]
   variables$n_files[i] = length(files.of.variable)
-  #x = read.csv(tail(files.of.variable,1), sep = ";") 
+  x = read.csv(tail(files.of.variable,1), sep = ",") 
   #names(x) = c("BTR", "Spor", "fra", "til", "date", "x")
-  #variables$resolution[i] = as.numeric(quantile(abs(diff(x$fra)), 0.5))
-  #print(paste("Loop", i, "has resolution", as.numeric(quantile(abs(diff(x$fra)), 0.5))))
+  variables$resolution[i] = as.numeric(quantile(abs(diff(x$fra)), 0.5))
+  print(paste("Loop", i, "has resolution", as.numeric(quantile(abs(diff(x$fra)), 0.5))))
 }
 sum(variables$n_files)
+
+allowed_resolutions <- c(0.00025, 0.001)
+variables$resolution <- sapply(variables$resolution, function(x) {
+  allowed_resolutions[which.min(abs(allowed_resolutions - x))]
+})
+variables
 
 
 #####################################################
 ### SPLIT MEASUREMENT DATA TO BTR SEPARATED FILES ###
 #####################################################
+
+
 destination = "C:/Users/ATBD/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/database"
+#destination = "C:/Users/askbi/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/database"
+#destination = "C:/Users/ATBD/Downloads/database"
+
+list.files(destination)
 
 btr_range = c(14000,14058)
 
-
-i = 2 #loops through all variables
-files.of.variable = all.all.files[grepl(variables$name[i], all.all.files, ignore.case = T)]
-dir.create(file.path(destination, variables$name[i]), showWarnings = FALSE)
-
-
-for(j in 1:length(files.of.variable)){ #loops through all campaigns for variable i
-  start_time <- Sys.time() #stop timer
-  date_nam = sub(".*Campaign_", "",files.of.variable[j])
-  x = read.csv(files.of.variable[j], sep = ";")
-  names(x) = c("BTR", "Spor", "fra", "til", "date", "x")
-  BTRs = unique(x$BTR)
-  chosenBTRs = BTRs[BTRs>=btr_range[1] & BTRs<=btr_range[2]]
-  x = x[x$BTR %in% chosenBTRs,]
+#04.03.25: RAN THE LOOP FROM i = 3:10; took roughly 35 min in average per iteration
+#05.03.25: RAN THE LOOP FROM i = 11:15; took roughly 1 hour and 20 minutes in average per iteration
+#06.03.25: RAN THE LOOP FROM i = 16:27; 
+#06.03.25: RAN THE LOOP FROM i = 28:34; 
+for(i in 35:35){
   
-  for(btr in chosenBTRs){
-    idx = x$BTR == as.numeric(btr) #extract rows with corresponding btr
-    nam = paste0("BTR_",btr,"_",variables$name[i], "_Campaign_", date_nam) #construct name
-    if(! nam %in% list.files(destination)){ #if not already saved, then create new file
-      write.csv(x[idx, ],file = file.path(destination, variables$name[i],nam), row.names=FALSE)
+  files.of.variable = all.all.files[grepl(variables$name[i], all.all.files, ignore.case = T)]
+  
+  dir_path = file.path(destination, variables$name[i])
+  if (!dir.exists(dir_path)) { dir.create(dir_path, showWarnings = FALSE) }
+  
+  start_time <- Sys.time() #start timer
+  for(j in 1:length(files.of.variable)){ #loops through all campaigns for variable i
+    date_nam = sub(".*Campaign_", "",files.of.variable[j])
+    
+    x = read.csv(files.of.variable[j], sep = ";")
+    names(x) = c("BTR", "Spor", "fra", "til", "date", "x")
+    BTRs = unique(x$BTR)
+    chosenBTRs = BTRs[BTRs>=btr_range[1] & BTRs<=btr_range[2]]
+    if(length((chosenBTRs)) == 0) next
+    x = x[x$BTR %in% chosenBTRs,]
+    
+    for(btr in chosenBTRs){
+      idx = x$BTR == as.numeric(btr) #extract rows with corresponding btr
+      nam = paste0("BTR_",btr,"_",variables$name[i], "_Campaign_", date_nam) #construct name
+      if(! nam %in% list.files(file.path(destination, variables$name[i]))){ #if not already saved, then create new file
+        write.csv(x[idx, ],file = file.path(destination, variables$name[i],nam), row.names=FALSE)
+      }
+      x = x[!idx, ] #remove extracted rows for better speeds
     }
-    x = x[!idx, ] #remove extracted rows for better speeds
   }
-  
   end_time <- Sys.time() #stop timer
   elapsed_time <- end_time - start_time
-  print(paste("File =", j, "::: Runtime =", elapsed_time))
+  print(paste("Variable =", i, "::: Runtime =", elapsed_time))
 }
+
 
 #remember to check if the correct data are pasted in the correct files
 
@@ -187,24 +212,33 @@ for(j in 1:length(files.of.variable)){ #loops through all campaigns for variable
 ### NOTE: After all measurement files for a pattern are run through, 
 ### a version of the data structure should be saved to a folder.
 
-all.files = list.files(paste0(path), full.names = T)
-pattern = "Hojd_L_D1"
-files = list.files(all.files[grepl(pattern, all.files, ignore.case = T)], full.names = T)
+convert_to_numeric <- function(vec) {
+  if (is.numeric(vec)) {
+    return(vec)  # Return as is if already numeric
+  } else {
+    return(as.numeric(gsub(",", ".", vec)))  # Convert string to numeric
+  }
+}
 
-res = 0.00025 #as.numeric(quantile(abs(x$fra - x$til), probs = 0.5)) #get spatial resolution
 
-for(i in 1:2){ #run through files with pattern
-  x = read.csv(files[i], sep = ";"); 
-  names(x) = c("BTR", "Spor", "fra", "til", "date", "x")
-  x_entire = x; #store initial data
+destination = "C:/Users/ATBD/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/database"
+folders = list.files(destination)
+
+for(i in 1:length(folders)){
+  start_time <- Sys.time() #stop timer
+  varnam = folders[i] #variable name
+  files = list.files(file.path(destination, folders[i]), full.names = T) #files of that variables  
+  res = variables[variables$name == varnam,"resolution"] #get resolution for file
   
-  for(btr in names(RosRing)){
-    for(trk in names(RosRing[[btr]])){
-      x = x_entire #reinstantiate measurement file
-      idx1 = x$BTR == as.numeric(btr) 
-      idx2 = x$Spor[idx1] == trk
-      x = x[idx1, ][idx2, ] #filtering so we only consider correct track and btr
-      
+  for(j in 1:length(files)){
+    btr=sub(".*BTR_(\\d+).*", "\\1", files[j]) #btr number
+    x = read.csv(files[j], sep = ","); x_entire = x; #read x
+    
+    for(trk in names(RosRing[[btr]])){#get different tracks
+      x = x_entire; #reinstanziate x
+      idx = x$Spor == trk #get track indices
+      if(sum(idx) == 0) next #print(paste("Check 1. Skipping", varnam, "BTR:", btr, "Track:", trk)); next
+      x = x[idx, ]
       pos1 = head(RosRing[[btr]][[trk]]$statics$fra,1) #range of btr and track
       pos2 = tail(RosRing[[btr]][[trk]]$statics$til,1) - res
       mesh = seq(pos1, pos2, by = res) #make mesh
@@ -212,8 +246,6 @@ for(i in 1:2){ #run through files with pattern
       #does x cover the the interval (pos1, pos2)
       cond1 = any(abs(x$fra-pos1) <= res)
       cond2 = any(abs(x$fra-pos2) <= res)
-      #cond1 = any((x$fra >= pos1 - res) & (x$fra <= pos1 + res))
-      #cond2 = any((x$fra >= pos2 - res) & (x$fra <= pos2 + res))
       if(cond1 & cond2){ # x covers the interval (pos1, pos2)
         idx1 = which.min(abs(x$fra-pos1))
         idx2 = which.min(abs(x$fra-pos2))
@@ -223,110 +255,60 @@ for(i in 1:2){ #run through files with pattern
         idx1 = which.min(abs(x$fra-val1))
         idx2 = which.min(abs(x$fra-val2))
       }
+      if(!(cond1 | cond2)) next #print(paste("Check 2. Skipping", varnam, "BTR:", btr, "Track:", trk)); next
+      if(idx2 < idx1) next #print(paste("Check 3. Skipping", varnam, "BTR:", btr, "Track:", trk)); next
+        
+      x = x[idx1:idx2, ]
+      x <- x[order(x$fra), ]
+      x <- x[!duplicated(x$fra), ]  # Remove duplicate rows based on 'fra'
+      x$x = convert_to_numeric(x$x)
       
-      if(cond1 | cond2){ #storage of values:
-        # x = x[idx1:idx2, ]
-        # 
-        # #how to put the relevant x values into the mesh
-        # meas = data.frame(pos = mesh, x = NA)
-        # start = which.min(abs(x$fra[1] - mesh))
-        # meas$x = NA
-        # 
-        # len_x = length(x$x)
-        # len_target = length(meas$x[start:(start + len_x - 1)])
-        # #meas$x[start:NROW(x)] = x$x
-        # if (len_x == len_target) {
-        #   meas$x[start:(start + len_x - 1)] = x$x
-        # } else {
-        #   warning("Mismatch in lengths: x$x has ", len_x, " elements, but target range has ", 
-        #           len_target, " elements in BTR ", btr, " track ", trk)
-        # }
-        
-        x = x[idx1:idx2, ]
-        
-        #Compute gaps and find large gaps > 0.0005
-        gaps = diff(x$fra)
-        gap_idx = which(gaps > (2*res))
-        
-        #Interpolation with NA for large gaps
-        interp_values = approx(x = x$fra, y = x$x, xout = mesh, method = "linear", rule = 2)
-        meas = data.frame(pos = mesh, x = interp_values$y)
-        
-        #Set NA for large gaps
-        for (idx in gap_idx) {
-          # Find positions in 'mesh' that fall within the large gap
-          missing_mask = (meas$pos > x$fra[idx]) & (meas$pos < x$fra[idx + 1])
-          meas$x[missing_mask] = NA  # Assign NA to those positions
-        }
-        
-        colnames(meas) = c("pos", x$date[1])
-        
-        #stack measurements nested list structure
-        if(length(RosRing[[btr]][[trk]]$meas) == 0){ #if there is no previous measurements
-          RosRing[[btr]][[trk]]$meas[[pattern]] = meas
-        } else{
-          RosRing[[btr]][[trk]]$meas[[pattern]] = cbind(RosRing[[btr]][[trk]]$meas[[pattern]], 
-                                                        meas[,2])
-          
-          colnames(RosRing[[btr]][[trk]]$meas[[pattern]])[NCOL(RosRing[[btr]][[trk]]$meas[[pattern]])] = x$date[1]
-        }
+      if(nrow(x) < 2 || all(is.na(x$x))) next #print(paste("Check 4. Skipping", varnam, "BTR:", btr, "Track:", trk)); next
+      
+      #Compute gaps and find large gaps
+      gaps = diff(x$fra)
+      gap_idx = which(gaps >= (2*res))
+      
+      #Interpolation with NA for large gaps
+      interp_values = approx(x = x$fra, y = x$x, xout = mesh, method = "linear", rule = 2)
+      meas = data.frame(pos = mesh, x = interp_values$y)
+      
+      #Set NA for large gaps
+      for (idx in gap_idx) {
+        # Find positions in 'mesh' that fall within the large gap
+        missing_mask = (meas$pos > x$fra[idx]) & (meas$pos < x$fra[idx + 1])
+        meas$x[missing_mask] = NA  # Assign NA to those positions
       }
-    }
+      colnames(meas) = c("pos", x$date[1])
+      
+      #stack measurements nested list structure
+      if(is.null(RosRing[[btr]][[trk]]$meas[[varnam]])){ #if there is no previous measurements
+        RosRing[[btr]][[trk]]$meas[[varnam]] = meas
+      } else{
+        RosRing[[btr]][[trk]]$meas[[varnam]] = cbind(RosRing[[btr]][[trk]]$meas[[varnam]],meas[,2])
+        colnames(RosRing[[btr]][[trk]]$meas[[varnam]])[NCOL(RosRing[[btr]][[trk]]$meas[[varnam]])] = x$date[1]
+      }
+    } 
   }
+  end_time <- Sys.time() #stop timer
+  elapsed_time <- end_time - start_time
+  print(paste("Variable", varnam, "took",elapsed_time))
 }
+
+
+#save(RosRing, file = "C:/Users/ATBD/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/RosRing.RData")
+
+
+
+#load(file="C:/Users/askbi/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/RosRing.RData")
+#load(file = "C:/Users/ATBD/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/RosRing.RData")
 
 
 btr = names(RosRing)[1]
 trk = names(RosRing[[btr]])[2]
+names(RosRing[[btr]][[trk]])
 
-tail(RosRing[[btr]][[trk]]$meas[[pattern]],20)
-
-RosRing[[btr]][[trk]]$statics
-
-object.size(RosRing)
-
-#needs check for
-# - what if there is no data added from a file
-# - what if partial data is added? Is it still correct? Probably, but you should check again
-# - what makes the code slow?
-
-
-
-# for(f in files){
-#   dates <- regmatches(f, gregexpr("\\d{4}-\\d{2}-\\d{2}", f))[[1]]
-#   print(diff(as.Date(dates)))
-#   x = read.csv(f, sep = ";")
-#   names(x) = c("BTR", "Spor", "fra", "til", "date", pattern)
-#   print(sum(x$til-x$fra))
-#   print(object.size(x)/1E6)
-# }
-# 
-# 
-# quantile(x$til-x$fra)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+x = RosRing[[btr]][[trk]]$meas
 
 
 
