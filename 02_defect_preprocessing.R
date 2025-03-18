f7cfcdc1fc7c36042147b8dca09082673192874e
dobbeltgaard <albert.bisgaard@gmail.com>
2025-03-18 18:14:04 +0100

    better filtering. Combination with maintenance data

--- 02_defect_preprocessing.R
+++ 02_defect_preprocessing.R
@@ -67,41 +67,87 @@ for(i in 1:length(IDs)){
   idx = d$Defect_ID3 == IDs[i]
   foo = d[idx,]
   x = split(foo,list(foo$Rail_string,foo$Defect_found_in), drop=TRUE) #split to unique defects, so rail string and defect_found_in surely matches
-  if(NROW(x) < 2){ defect_set[[toString(IDs[i])]] = x[[1]]; next} #if data correct, then no splitting is needed
+  
   for(j in 1:NROW(x)){
-    nam = paste0(sprintf("%.0f", 100000000),toString(IDs[i]),j) #naming of split defect
-    defect_set[[nam]] = x[[j]]
+    if(NROW(x) < 2){ nam = toString(IDs[i])} else {nam = paste0(sprintf("%.0f", 100000000),toString(IDs[i]),j) }# defect_set[[toString(IDs[i])]] = x[[1]][order(x[[1]]$Date),]; next} #x[[1]]; next} #if data correct, then no splitting is needed
+    foo2 = x[[j]][order(x[[j]]$Date),]
+    
+    if(NROW(foo2) > 1){
+      idxfoo2 = rep(T, NROW(foo2));  
+      if(any(head(foo2$State == "Removed",-1)) & !all(foo2$State == "Removed")){ idxfoo2[foo2$State == "Removed"] = F; idxfoo2[length(idxfoo2)]=T;} #filter out observations with "Removed" under state, excect from endpoint
+      foo2 = foo2[idxfoo2, ]
+      if(sum(idxfoo2) > 1){ #if more than 1 obs is left, then check for time differences
+        idx2foo2 = rep(T, NROW(foo2));
+        idx2foo2 = outer(foo2$Date, foo2$Date, FUN = difftime, units = "days")[,1] > 30 #only obs with bigger distance than 30 days
+        idx2foo2[!idx2foo2][which.max(foo2$Date[!idx2foo2])] = T
+        foo2 = foo2[idx2foo2, ]
+      }
+    }
+    foo2$Maintenance_date = NA
+    foo2$Maintenance_type = NA
+    foo2$Amount = NA
+    defect_set[[nam]] = foo2
   }
 }
 names(defect_set) = as.numeric(names(defect_set))
 
+#save(defect_set, file = "C:/Users/ATBD/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/Defect_set_without_maintenance.RData")
+load("C:/Users/ATBD/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/Defect_set_without_maintenance.RData")
+
+#################################################
+### COMBINE DEFECT DATA WITH MAINTENANCE DATA ###
+#################################################
+main = read.csv(file = "rail_maintenance.csv")
+for(i in 1:length(defect_set)){
+  if(NROW(defect_set[[i]])>1){ #if there are more than one observation
+    idx = defect_set[[i]]$BTR[1] == main$BTR & defect_set[[i]]$Track[1] == main$Track
+    cond1 = defect_set[[i]]$From[1] >= main$From[idx] & defect_set[[i]]$From[1] <= main$To[idx]
+    cond2 = defect_set[[i]]$To[1] >= main$From[idx] & defect_set[[i]]$To[1] <= main$To[idx]
+    foo = main[idx,][cond1 | cond2,]
+    if(NROW(foo)>0){
+      for(j in 1:NROW(foo)){
+        cond3 = foo$Time[j] > defect_set[[i]]$Date  
+        cond4 = foo$Time[j] < defect_set[[i]]$Date
+        if(any(cond3) & any(cond4)){ #if maintenance is between 
+          defect_set[[i]][which(cond4)[1],c("Maintenance_date","Maintenance_type","Amount")] =
+            foo[j,c("Time", "action","removed_mm") ]
+        }
+      }
+    }
+  }
+}
+#save(defect_set, file = "C:/Users/ATBD/OneDrive - Danmarks Tekniske Universitet/SpatioTemporal/Defect_set_with_maintenance.RData")
+
+# COMBINE with static data. Tonnage. Speed. Curveture.
 
 
-
-M = matrix(NA, nrow = length(defect_set), ncol = 3)
+M = matrix(NA, nrow = length(defect_set), ncol = 5)
 for(i in 1:length(defect_set)){
   foo = defect_set[[i]]
   M[i,1] = NROW(foo)
-  M[i,2] = max(table(foo$Defect_found_in))
-  M[i,3] = min(c(max(table(foo$Defect_found_in)),
-                 max(table(foo$Rail_string))
-                 )
-               )
+  if(NROW(foo) < 2){M[i,2] = F;  M[i,3] = F; M[i,4] = F; } 
+  M[i,2] = foo$UIC[length(foo$UIC)]
+  M[i,3] = any(!is.na(foo$Maintenance_date))
   }
+
 
 
 table(M[,1])
 table(M[,2])
-which(M[,3] ==10)
+table(M[,3])
 
 
-(defect_set[[8939    ]])
+which(M[,1] ==3)
 
 
 
+which(M[,3] == 1)
 
-#sort after dates
-#State: Removed. 
+defect_set[[3749 ]]
+
+
+#incorporate maintenance
+
 
 #Before sending to Xiaokun: Only send defects with more than one obs. Remember to remove unneeded columns for better readability.
 
